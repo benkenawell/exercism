5b0648b38f06f5615a1336daf2a3581116fae175
Ben Kenawell <ben@benkenawell.com>
2020-01-01 19:03:07 -0800

    completed with iterators

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,12 +1,11 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    let mut sum = Vec::new();
-    for i in 0..limit {
+    (0..limit).fold(Vec::new(), |mut list_of_multiples, i| {
         for factor in factors {
             let multiple = factor * i;
-            if multiple < limit && !sum.contains(&multiple) {
-                sum.push(multiple)
+            if multiple < limit && !list_of_multiples.contains(&multiple) {
+                list_of_multiples.push(multiple)
             }
         }
-    }
-    sum.iter().fold(0, |sum, x| sum + x)
+        list_of_multiples
+    }).iter().fold(0, |sum, x| sum + x)
 }
