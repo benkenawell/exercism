594a44395dfd36d830ae5e6949946579bad34f89
Ben Kenawell <ben@benkenawell.com>
2020-01-04 00:41:11 -0800

    redid with more iterators

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,10 +1,12 @@
 pub fn is_armstrong_number(num: u32) -> bool {
     let numstr = num.to_string();
-    let digits: Vec<&str> = numstr.split("").filter(|x| x.len() > 0).collect();
-    let sum_of_digits = (0..digits.len()).fold(0, |acc, idx| {
-        let base: u32 = digits[idx].parse().expect("A number!");
-        let power: u32 = digits.len() as u32;
-        acc + base.pow(power)
-    });
+    let sum_of_digits = numstr
+        .split("")
+        .filter(|x| x.len() > 0)
+        .fold(0, |acc, digit| {
+            let base: u32 = digit.parse().expect("A number!");
+            let power: u32 = numstr.len() as u32;
+            acc + base.pow(power)
+        });
     num == sum_of_digits
 }
