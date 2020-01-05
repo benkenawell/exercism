e1cf859d927f5274be1ded5218474345a4c84ca8
Ben Kenawell <ben@benkenawell.com>
2020-01-04 22:03:51 -0800

    completed series

--- rust/series/src/lib.rs
+++ rust/series/src/lib.rs
@@ -1,7 +1,10 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    unimplemented!(
-        "What are the series of length {} in string {:?}",
-        len,
-        digits
-    )
+    // If the user wants a string longer than the string they gave us,
+    // return an empty Vec
+    if len > digits.len() { return Vec::<String>::new(); }
+    // otherwise, calculate all the strings for them
+    (0..(digits.len()-len+1))
+        .map(|idx| digits.get(idx..idx+len).expect("String should have been this long!"))
+        .map(|string| String::from(string)) // map &str to String
+        .collect()
 }
