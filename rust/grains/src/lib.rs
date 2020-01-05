48b96021decab5b469a538a30557d66383883663
Ben Kenawell <ben@benkenawell.com>
2020-01-04 19:50:56 -0800

    solved with math

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,8 +1,7 @@
 pub fn square(s: u32) -> u64 {
     if s <= 0 || s > 64 { panic!("Square must be between 1 and 64"); }
-    // if the quare is on a chess board
-    if s == 1 { 1 }
-    else { 2 * square(s-1) }
+    // if the square is on a chess board
+    1 * 2_u64.pow(s-1)
 }
 
 pub fn total() -> u64 {
