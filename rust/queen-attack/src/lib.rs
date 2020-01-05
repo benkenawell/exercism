3488403ea785d9f9760f2c731e6bb04811f071a1
Ben Kenawell <ben@benkenawell.com>
2020-01-04 22:32:57 -0800

    completed queen-attack

--- rust/queen-attack/src/lib.rs
+++ rust/queen-attack/src/lib.rs
@@ -1,31 +1,41 @@
 #[derive(Debug)]
-pub struct ChessPosition;
+pub struct ChessPosition (i32, i32);
 
 #[derive(Debug)]
-pub struct Queen;
+pub struct Queen {
+    position: ChessPosition
+}
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        unimplemented!(
-            "Construct a ChessPosition struct, given the following rank, file: ({}, {}). If the position is invalid return None.",
-            rank,
-            file
-        );
+        if rank >= 0 && rank < 8 && file >= 0 && file < 8 {
+            Some(ChessPosition (rank, file))
+        } else {
+            None
+        }
+    }
+
+    pub fn same_rank(&self, other: &ChessPosition) -> bool {
+        self.0 == other.0
+    }
+
+    pub fn same_file(&self, other: &ChessPosition) -> bool {
+        self.1 == other.1
+    }
+
+    pub fn same_diag(&self, other: &ChessPosition) -> bool {
+        (self.0 - other.0).abs() == (self.1 - other.1).abs()
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        unimplemented!(
-            "Given the chess position {:?}, construct a Queen struct.",
-            position
-        );
+        Queen { position: position }
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        unimplemented!(
-            "Determine if this Queen can attack the other Queen {:?}",
-            other
-        );
+        self.position.same_rank(&other.position)
+        || self.position.same_file(&other.position)
+        || self.position.same_diag(&other.position)
     }
 }
