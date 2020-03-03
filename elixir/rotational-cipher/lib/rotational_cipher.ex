261f08f56ff32790e1d196a6404b4e29cc09891d
Ben Kenawell <ben@benkenawell.com>
2020-03-02 20:31:31 -0800

    completed elixir rot cipher

--- elixir/rotational-cipher/lib/rotational_cipher.ex
+++ elixir/rotational-cipher/lib/rotational_cipher.ex
@@ -8,5 +8,15 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+  	String.codepoints(text) |> Enum.map(&(rotate_char(&1, shift))) |> Enum.join("")
   end
+
+	def rotate_char(<<ch>>, shift) do
+		case ch do
+			x when x > 64 and x < 91 -> <<rem(ch - 64 + shift,26) + 64>>
+			x when x > 96 and x < 123 -> <<rem(ch - 97 + shift,26) + 97>>
+			x -> <<x>>
+		end
+	end
+
 end
