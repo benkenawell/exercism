b9432d209276054ffa6803d7198043ebc01ab0fe
Ben Kenawell <ben@benkenawell.com>
2020-04-11 22:27:55 -0700

    first working solution

--- purescript/rna-transcription/src/RNATranscription.purs
+++ purescript/rna-transcription/src/RNATranscription.purs
@@ -1,3 +1,22 @@
 module RNATranscription
   ( toRNA
   ) where
+
+import Prelude
+import Data.String (split, joinWith, Pattern(..))
+import Data.Maybe
+import Data.Functor ((<#>))
+import Data.Array (foldM)
+
+toRNA :: String -> Maybe String
+toRNA rna = split (Pattern "") rna # foldM foldRNA ""
+
+foldRNA :: String -> String -> Maybe String
+foldRNA acc rna = (rotateRNA rna) >>= (\rotRNA -> Just ( acc <> rotRNA))
+
+rotateRNA :: String -> Maybe String
+rotateRNA "G" = Just "C"
+rotateRNA "C" = Just "G"
+rotateRNA "A" = Just "U"
+rotateRNA "T" = Just "A"
+rotateRNA x = Nothing
