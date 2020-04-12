2db5290c4ce1b944cb003162c671ee3503b4b11e
Ben Kenawell <ben@benkenawell.com>
2020-04-12 00:25:38 -0700

    reorganized, added comments, and cleaned up imports

--- purescript/rna-transcription/src/RNATranscription.purs
+++ purescript/rna-transcription/src/RNATranscription.purs
@@ -2,22 +2,24 @@ module RNATranscription
   ( toRNA
   ) where
 
-import Prelude
-import Data.String (split, joinWith, Pattern(..))
+import Prelude (($), (<$>), append)
+import Data.String (split, Pattern(..))
 import Data.Maybe (Maybe(..))
 import Data.Array (foldM)
 
+-- folds the RNA rotation on every given letter.
+-- returns Nothing if there are any invalid characters
 toRNA :: String -> Maybe String
-toRNA rna = split (Pattern "") rna # foldM foldRNA ""
+toRNA rna =  foldM foldRNA "" $ split (Pattern "") rna
 
-foldRNA :: String -> String -> Maybe String
--- foldRNA acc rna = (rotateRNA rna) >>= (\rotRNA -> Just ( acc <> rotRNA))
--- foldRNA acc rna = rotateRNA rna # \rotRNA -> append acc <$> rotRNA
-foldRNA acc rna = append acc <$> rotateRNA rna
-
+-- performs the RNA rotation on a single letter
 rotateRNA :: String -> Maybe String
 rotateRNA "G" = Just "C"
 rotateRNA "C" = Just "G"
 rotateRNA "A" = Just "U"
 rotateRNA "T" = Just "A"
 rotateRNA x = Nothing
+
+-- utility function to facilitate fold
+foldRNA :: String -> String -> Maybe String
+foldRNA acc rna = append acc <$> rotateRNA rna
